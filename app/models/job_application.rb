ba730990b08aae36ec8024a9a9b45877b03d81a2
Dillon <dilloncortez@gmail.com>
2017-01-23 11:30:22 -0700

    updated mechanize gem

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -24,23 +24,25 @@
 
 require 'capybara/poltergeist'
 require 'open-uri'
-require 'watir-webdriver'
-require 'watir-webdriver/wait'
+require 'watir'
+
+# require 'watir-webdriver'
+# require 'watir-webdriver/wait'
 require 'headless'
 
 class JobApplication < ActiveRecord::Base
   belongs_to :job_link
-  # after_save :apply_to_job, unless: :applied_to 
+  # after_save :apply_to_job, unless: :applied_to
 
    def fill_out_modal_with_text_first(input_frame)
-    fill_out_text_form(input_frame) 
+    fill_out_text_form(input_frame)
     if input_frame.button(id: 'apply').present?
       input_frame.button(id: 'apply').click
       puts "applied"
-    else  
+    else
       input_frame.a(class: 'button_content', text: 'Continue').when_present.click
       click_checkboxes(input_frame)
-      if input_frame.button(id: 'apply').present? 
+      if input_frame.button(id: 'apply').present?
         input_frame.button(id: 'apply').click
         puts "applied"
       else
@@ -48,9 +50,9 @@ class JobApplication < ActiveRecord::Base
         click_checkboxes(input_frame)
         input_frame.button(id: 'apply').click
         puts "applied"
-      end  
-    end        
-  end 
+      end
+    end
+  end
 
 
   def fill_out_modal_with_text_last(input_frame)
@@ -65,15 +67,15 @@ class JobApplication < ActiveRecord::Base
     else
       fill_out_text_like_a_human(input_frame.text_field(id: 'applicant.firstName'), user_name.split(' ').first)
       fill_out_text_like_a_human(input_frame.text_field(id: 'applicant.lastName'), user_name.split(' ').last)
-    end  
-  end 
+    end
+  end
 
   def fill_out_text_like_a_human(field, text)
     text_array = text.split('', 4)
     text_array.each {|letters| field.set letters }
     field.set text
-  end  
-  
+  end
+
 
   def fill_out_text_form(input_frame)
     puts "filling out name"
@@ -87,11 +89,11 @@ class JobApplication < ActiveRecord::Base
     puts "writing cover letter"
     fill_out_text_like_a_human(input_frame.text_field(id: 'applicant.applicationMessage'), user_cover_letter)
     # byebug
-  end  
+  end
 
   def click_checkboxes(input_frame)
     puts "checkin boxes"
-    input_frame.div(id: "q_0").when_present do 
+    input_frame.div(id: "q_0").when_present do
       puts "radio frame is present"
       %w(0 1 2 3 4).each do |question_number|
         if input_frame.div(id: "q_#{question_number}").present?
@@ -103,7 +105,7 @@ class JobApplication < ActiveRecord::Base
       end
     end
     puts "boxes checked"
-  end 
+  end
 
   def open_modal(browser)
     puts "not found"
@@ -122,78 +124,77 @@ class JobApplication < ActiveRecord::Base
     elsif browser.span(class: "indeed-apply-button-inner").exists?
       puts "found at browser.span(class: 'indeed-apply-button-inner')"
       browser.span(class: "indeed-apply-button-inner").click
-    else 
-      puts "cant find" 
-      false  
-    end  
-  end 
+    else
+      puts "cant find"
+      false
+    end
+  end
 
   def apply_to_job
     @counter = 0
     if self.should_apply == true  
       until self.applied_to == true || @counter == 3
-        begin 
+        begin
         puts "\n\n\n\n\n#{'∞∞∞∞∞∞∞'*20}\n\n#{indeed_link} ---------- id: #{id}\n\n\n\n"
 
         browser = Watir::Browser.new :phantomjs, :args => ['--ssl-protocol=tlsv1']
         # browser.driver.manage.timeouts.implicit_wait = 3 #3 seconds
         browser.goto indeed_link
-        if open_modal(browser)  
+        if open_modal(browser)
           puts "clicked modal button"
           sleep 3.5
           if browser.iframe(id: /indeed-ia/).exists?
             puts "found"
             # byebug
             input_frame = browser.iframe(id: /indeed-ia/).iframe
-              if input_frame.text_field(id: 'applicant.name').present? || input_frame.text_field(id: 'applicant.firstName').present?  
-                  fill_out_modal_with_text_first(input_frame)        
+              if input_frame.text_field(id: 'applicant.name').present? || input_frame.text_field(id: 'applicant.firstName').present?
+                  fill_out_modal_with_text_first(input_frame)
               else
                 fill_out_modal_with_text_last(input_frame)
-              end  
-              self.update(applied_to: true) 
-          end    
+              end
+              self.update(applied_to: true)
+          end
         end
         rescue => e
           puts e
-        end  
+        end
         clean_up_temporary_binary_file
         browser.close
         @counter += 1
       end
-    end    
+    end
   end
 
   def user_resume
     Aws.config.update({
       region: 'us-east-1',
       credentials: Aws::Credentials.new(ENV['AMAZON_ACCESS_KEY_ID'], ENV['AMAZON_SECRET_ACCESS_KEY'])
-    }) 
+    })
     s3 = Aws::S3::Client.new
     create_temp_file
     store_and_return_user_resume(s3)
-  end 
+  end
 
   def create_temp_file
     File.new(local_file_path, "w+")
-  end  
+  end
 
   def store_and_return_user_resume(s3)
     File.open(local_file_path, "w+") { |f| f.write(resume_object(s3).read)}
     File.absolute_path(local_file_path)
   end
-  
+
   def local_file_path
     "public/#{user_resume_path.split('/').last}"
-  end  
+  end
 
   def clean_up_temporary_binary_file
     File.delete(local_file_path) if File.exist?(local_file_path)
-  end  
+  end
 
 
   def resume_object(s3)
     s3.get_object(bucket: 'job-bot-bucket', key: user_resume_path).body
-  end 
+  end
 
 end
-
