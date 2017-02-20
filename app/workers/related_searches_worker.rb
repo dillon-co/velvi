793a543cac19fa2351e4434140ce5a910dc0bb78
Dillon <dilloncortez@gmail.com>
2017-02-20 16:15:06 -0700

    fixed bug again

--- app/workers/related_searches_worker.rb
+++ app/workers/related_searches_worker.rb
@@ -4,7 +4,7 @@ class RelatedSearchesWorker
     j = JobLink.find(p['j'].to_i)
     location, skill = j.job_location, j.skill_level
     puts "\n\n#{p}\n\n"
-    p['related_searches'].each do |s|
+    p['r_searches'].each do |s|
       jl = JobLlink.create(job_title: s, job_location: location, skill_level: skill)
       jl.call_search_worker
     end
