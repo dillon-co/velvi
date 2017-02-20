9bc0d9c3c252ceda3af58093fc8e09e625be245d
Dillon <dilloncortez@gmail.com>
2017-02-20 16:04:08 -0700

    fixed bug again

--- app/workers/related_searches_worker.rb
+++ app/workers/related_searches_worker.rb
@@ -2,7 +2,7 @@ class RelatedSearchesWorker
   include Sidekiq::Worker
   def perform(p)
     j = JobLink.find(p['j'].to_i)
-    location, skill = j.location, j.skill_level
+    location, skill = j.job_location, j.skill_level
     p['related_searches'].each do |s|
       jl = JobLlink.create(job_title: s, job_location: location, skill_level: skill)
       jl.call_search_worker
