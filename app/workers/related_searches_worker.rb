8619f07ea2e95b6bb22e6547e064deae5f2fa991
Dillon <dilloncortez@gmail.com>
2017-02-26 12:20:39 -0700

    updated credits

--- app/workers/related_searches_worker.rb
+++ app/workers/related_searches_worker.rb
@@ -6,9 +6,14 @@ class RelatedSearchesWorker
     u = j.user
     location, skill = j.job_location, j.skill_level
     puts "\n\n#{p}\n\n"
+    u.update(credits: updated_credits(u, p['r_searches'].length))
     p['r_searches'].each do |s|
       jl = u.job_links.create(job_title: s, job_location: location, skill_level: skill)
       jl.call_search_worker
     end
+  end
+
+  def updated_credits(user, credits_used)
+    user.credits - credits_used
   end
 end
